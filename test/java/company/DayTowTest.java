234a6f1e1661df6e7d61c21b022d8d3a138dfa8f
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-12-20 18:27:56 +0100

    +

--- test/java/company/DayTowTest.java
+++ test/java/company/DayTowTest.java
@@ -1,7 +1,38 @@
 package company;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DayTowTest {
 
+    @Test
+    void testExemple(){
+        String path="resources/day2Test.txt";
+        List<String> input=TestUtils.readFileIntoList(path);
+
+        int result=DayTow.submarine(input);
+
+        System.out.println(result);
+        assertEquals(5,result);
+
+
+
+    }
+
+    @Test
+    void testExemple2(){
+        String path="resources/day1.txt";
+        List<String> input=TestUtils.readFileIntoList(path);
+
+        int result=DayTow.submarine(input);
+
+        System.out.println(result);
+        //  assertEquals(7,result);
+
+
+
+    }
 }
